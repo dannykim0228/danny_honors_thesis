43e5930c8c643c73753dd002f2c440eae54646db
Aaron Abkemeier <aaronabkemeier@gmail.com>
2024-10-23 11:07:29 -0400

    1d_global example for Jun

--- src/1d_global_danny.py
+++ src/1d_global_danny.py
@@ -3,10 +3,38 @@ import jax.numpy as jnp
 from jax import random, grad, jit
 import pandas as pd
 import numpy as np
-import multiprocessing as mp
+import datetime
+import pypomp
+import pypomp.fit
+import pypomp.pomp_class
+#import multiprocessing as mp
+
+print("Current system time:", datetime.datetime.now())
+
+ncores = 2
+print(ncores)
+RUN_LEVEL = 1
+match RUN_LEVEL:
+    case 1:
+        NP_FITR = 2
+        NFITR = 2
+        NREPS_FITR = ncores
+        NP_EVAL = 2
+        NREPS_EVAL = ncores
+        NREPS_EVAL2 = ncores
+        print("Running at level 1")
+    case 2:
+        NP_FITR = 1000
+        NFITR = 200
+        NREPS_FITR = ncores
+        NP_EVAL = 5000
+        NREPS_EVAL = ncores
+        NREPS_EVAL2 = ncores*8
+        print("Running at level 2")
+
 
 # Data Manipulation
-sp500_raw = pd.read_csv("../data/SPX.csv")
+sp500_raw = pd.read_csv("data/SPX.csv")
 sp500 = sp500_raw.copy()
 sp500['date'] = pd.to_datetime(sp500['Date'])
 sp500['diff_days'] = (sp500['date'] - sp500['date'].min()).dt.days
@@ -21,15 +49,15 @@ sp500_ivp_names = ["V_0"]
 sp500_parameters = sp500_rp_names + sp500_ivp_names
 sp500_covarnames = ["covaryt"]
 
-def rproc(state, covaryt, params, key):
+def rproc(state, params, key, covars = None):
     V, S = state
-    mu, kappa, theta, xi, rho = params
+    mu, kappa, theta, xi, rho, V_0 = params
     
     # Wiener process generation (Gaussian noise)
     dZ = random.normal(key)
     
     # Calculate dWs
-    dWs = (covaryt - mu + 0.5 * V) / jnp.sqrt(V)
+    dWs = (covars - mu + 0.5 * V) / jnp.sqrt(V)
     
     # dWv with correlation
     dWv = rho * dWs + jnp.sqrt(1 - rho ** 2) * dZ
@@ -39,13 +67,55 @@ def rproc(state, covaryt, params, key):
     V = V + xi * jnp.sqrt(V) * dWv
     
     # Feller condition to keep V positive
-    V = jnp.maximum(V, 1e-32)
+    # V = jnp.maximum(V, 1e-32)
     
     return V, S
 
 # Initialization Model
-def rinit(params):
-    V_0 = params["V_0"]
-    S_0 = 1105  # Initial price
+def rinit(params, J, covars = None):
+    # V_0 = params[5]
+    # S_0 = 1105  # Initial price
+    V_0 = jnp.repeat(params[5], J)
+    S_0 = jnp.repeat(1105, J)
     return V_0, S_0
 
+# Measurement model: how we measure state
+def dmeasure(y, state, params):
+    V, S = state
+    mu = params[0]
+    return jax.scipy.stats.norm.logpdf(y, mu - 0.5 * V, jnp.sqrt(V))
+
+# Initial parameter values
+#initial_params = {
+#    "mu": 0.01, "kappa": 0.1, "theta": 0.05, "xi": 0.1, "rho": 0.5, "V_0": 0.1
+#}
+
+initial_params = jnp.array([0.01, 0.1, 0.05, 0.1, 0.5, 0.1])
+
+# Initialize POMP model
+sp500_model = pypomp.pomp_class.Pomp(
+    rinit = rinit,
+    rproc = rproc,
+    dmeas = dmeasure,
+
+    # Observed log returns
+    ys = jnp.array(sp500['y'].values),
+
+    # Initial parameters
+    theta = initial_params,
+
+    # Covariates(time)
+    covars = jnp.insert(sp500['y'].values, 0, 0)
+)
+
+pypomp.fit.fit(
+    pomp_object = sp500_model,
+    J = NP_FITR,
+    Jh = 5,
+    M = 2,
+    a = 0.5,
+    itns = 2,
+    sigmas = 0.02,
+    sigmas_init = 1e-20,
+    mode = "IF2"
+)
