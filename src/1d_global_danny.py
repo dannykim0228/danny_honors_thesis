f28028e67f9c557fad2dc53c0a2c3075f6187919
Aaron Abkemeier <aaronabkemeier@gmail.com>
2024-11-01 16:12:59 -0400

    Basic 1d search file working

--- src/1d_global_danny.py
+++ src/1d_global_danny.py
@@ -1,6 +1,7 @@
+"""pypomp implementation of Weizhe's 1d_global_search.R code."""
 import jax
 import jax.numpy as jnp
-from jax import random, grad, jit
+from jax import random
 import pandas as pd
 import numpy as np
 import datetime
@@ -32,7 +33,6 @@ match RUN_LEVEL:
         NREPS_EVAL2 = ncores*8
         print("Running at level 2")
 
-
 # Data Manipulation
 sp500_raw = pd.read_csv("data/SPX.csv")
 sp500 = sp500_raw.copy()
@@ -50,72 +50,81 @@ sp500_parameters = sp500_rp_names + sp500_ivp_names
 sp500_covarnames = ["covaryt"]
 
 def rproc(state, params, key, covars = None):
-    V, S = state
+    """Process simulator for Weizhe model."""
+    V, S, t = state
     mu, kappa, theta, xi, rho, V_0 = params
+    mu = jnp.exp(mu)
+    xi = jnp.exp(xi)
+    rho = -1 + 2/(1 + jnp.exp(-rho))
+    t = t.astype(int)
     
     # Wiener process generation (Gaussian noise)
     dZ = random.normal(key)
     
     # Calculate dWs
-    dWs = (covars - mu + 0.5 * V) / jnp.sqrt(V)
-    
+    dWs = (covars[t] - mu + 0.5 * V) / jnp.sqrt(V)
+
     # dWv with correlation
     dWv = rho * dWs + jnp.sqrt(1 - rho ** 2) * dZ
     
     # Update state variables
     S = S + S * (mu + jnp.sqrt(jnp.maximum(V, 0.0)) * dWs)
+    #S = S + S * (mu + jnp.sqrt(V) * dWs)
     V = V + xi * jnp.sqrt(V) * dWv
+    t += 1
     
     # Feller condition to keep V positive
-    # V = jnp.maximum(V, 1e-32)
+    V = jnp.maximum(V, 1e-32)
     
-    return V, S
+    return jnp.array([V, S, t])
 
 # Initialization Model
 def rinit(params, J, covars = None):
-    # V_0 = params[5]
-    # S_0 = 1105  # Initial price
-    V_0 = jnp.repeat(params[5], J)
-    S_0 = jnp.repeat(1105, J)
-    return V_0, S_0
+    """Initial state process simulator for Weizhe model."""
+    V_0 = jnp.exp(params[5])
+    S_0 = 1105  # Initial price
+    t = 0
+    return jnp.tile(jnp.array([V_0, S_0, t]), (J,1))
 
 # Measurement model: how we measure state
 def dmeasure(y, state, params):
-    V, S = state
-    mu = params[0]
+    """Measurement model distribution for Weizhe model."""
+    V, S, t = state
+    mu = jnp.exp(params[0])
     return jax.scipy.stats.norm.logpdf(y, mu - 0.5 * V, jnp.sqrt(V))
 
-# Initial parameter values
-#initial_params = {
-#    "mu": 0.01, "kappa": 0.1, "theta": 0.05, "xi": 0.1, "rho": 0.5, "V_0": 0.1
-#}
-
-initial_params = jnp.array([0.01, 0.1, 0.05, 0.1, 0.5, 0.1])
+initial_params = jnp.array([
+    jnp.log(0.01), # mu
+    jnp.log(0.1), # kappa
+    jnp.log(0.05), # theta
+    jnp.log(0.1), # xi
+    jnp.log((1 + 0.5)/(1 - 0.5)), # rho 
+    jnp.log(0.1) # V_0
+])
 
 # Initialize POMP model
 sp500_model = pypomp.pomp_class.Pomp(
     rinit = rinit,
     rproc = rproc,
     dmeas = dmeasure,
-
     # Observed log returns
     ys = jnp.array(sp500['y'].values),
-
     # Initial parameters
     theta = initial_params,
-
     # Covariates(time)
     covars = jnp.insert(sp500['y'].values, 0, 0)
 )
 
-pypomp.fit.fit(
+fit_out = pypomp.fit.fit(
     pomp_object = sp500_model,
     J = NP_FITR,
     Jh = 5,
-    M = 2,
+    M = NREPS_FITR,
     a = 0.5,
     itns = 2,
-    sigmas = 0.02,
+    sigmas = 0.001,
     sigmas_init = 1e-20,
     mode = "IF2"
 )
+
+np.exp(fit_out[1][0])
